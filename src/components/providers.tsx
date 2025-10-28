c3f0c191a134b926a7485a1c0fb7988dd7d752a6
Sax Dhiut <mail@iyxeyl.me>
2025-10-28 15:26:46 +0400

    feat: add Toaster component to Providers for enhanced user notifications

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -2,7 +2,13 @@
 
 import type { ThemeProviderProps } from "next-themes"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { Toaster } from "@/components/ui/sonner"
 
 export function Providers({ children, ...props }: ThemeProviderProps) {
-	return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+	return (
+		<NextThemesProvider {...props}>
+			{children}
+			<Toaster />
+		</NextThemesProvider>
+	)
 }
