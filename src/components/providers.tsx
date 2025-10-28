c18ad475229de7c496919f0a358b1feb39d537ab
Sax Dhiut <mail@iyxeyl.me>
2025-10-28 15:01:48 +0400

    refactor: remove unused authentication and database files, streamline package.json scripts

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -2,12 +2,7 @@
 
 import type { ThemeProviderProps } from "next-themes"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { TRPCReactProvider } from "@/components/providers/trpc-provider"
 
 export function Providers({ children, ...props }: ThemeProviderProps) {
-	return (
-		<NextThemesProvider {...props}>
-			<TRPCReactProvider>{children}</TRPCReactProvider>
-		</NextThemesProvider>
-	)
+	return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
